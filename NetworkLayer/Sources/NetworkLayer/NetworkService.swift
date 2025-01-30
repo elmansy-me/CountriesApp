860f9a1b8282531ab2ef8addbc750e8f475c35d7
Ahmed Elmansy <elmansy.me@icloud.com>
2025-01-30 04:27:21 +0200

    feat(network-layer) Add method overloading for request API call function

--- NetworkLayer/Sources/NetworkLayer/NetworkService.swift
+++ NetworkLayer/Sources/NetworkLayer/NetworkService.swift
@@ -15,6 +15,20 @@ public class NetworkService {
     }
 
     public func request<T: Decodable>(_ apiRequest: APIRequest, responseType: T.Type) async throws -> T {
+        let data = try await fetchData(apiRequest)
+        
+        do {
+            return try JSONDecoder().decode(T.self, from: data)
+        } catch {
+            throw NetworkError.decodingError(error)
+        }
+    }
+    
+    public func request(_ apiRequest: APIRequest) async throws -> Data {
+        return try await fetchData(apiRequest)
+    }
+    
+    private func fetchData(_ apiRequest: APIRequest) async throws -> Data {
         let request = try apiRequest.buildURLRequest()
         
         do {
@@ -28,11 +42,7 @@ public class NetworkService {
                 throw NetworkError.statusCode(httpResponse.statusCode)
             }
             
-            do {
-                return try JSONDecoder().decode(T.self, from: data)
-            } catch {
-                throw NetworkError.decodingError(error)
-            }
+            return data
         } catch {
             throw NetworkError.unknown(error)
         }
